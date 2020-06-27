93c876954afe87f08c3531b81eb33e7fdbbe1506
Léo <leomiranda1995@gmail.com>
2020-06-27 20:55:39 -0300

    Tela de Cadastro adicionada

---
+++ app/src/main/java/com/example/android_leonardo/activity_cadastro.java
@@ -0,0 +1,14 @@
+package com.example.android_leonardo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
